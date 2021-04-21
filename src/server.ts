c119867656856b2425134c29dab0c78b18a9a2f1
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-04-21 00:59:01 -0300

    added get route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/', () => {
-    
-})
+app.get("/", (request, response) => {
+	return response.json({
+		message: 'welcome'
+	})
+});
 
-app.listen(3333, () => console.log('connected'))
+app.listen(3333, () => console.log("connected"));
